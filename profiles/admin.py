fcf2982089562c4b538cd7b43f7e5fdf97b7fa9c
Alan Murphy <alanjmurphy91@gmail.com>
2018-01-30 10:31:49 +0000

    added css

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Employee, Credit, Invoice, Analytics
+
 
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(Credit)
+admin.site.register(Invoice)
+admin.site.register(Analytics)
--- vb/settings.py
+++ vb/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    'blog_prj/static/',
+    'vb/vb/static/',
 )
 
 AUTHENTICATION_BACKENDS = (
